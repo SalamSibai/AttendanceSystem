eabc398e3f26d94378eea1d2a6aba71cc9047e87
Salam Nabil Sibai <salam.sibai@hotmail.com>
2020-05-19 23:14:52 +0400

    realCamSetup

---
+++ 3D Attendance System/Assets/ImageConverter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Text; 
+using UnityEngine.UI; 
+
+public class ImageConverter : MonoBehaviour
+{
+    public Texture2D myTexture;
+    byte[] bytes;
+    string encodedText; 
+    public RawImage pic; 
+
+
+    void Start()
+    {
+        bytes = myTexture.EncodeToJPG();    //encodes to a jpg byte array.
+        //encodedText = Convert.ToBase64String(bytes);
+        
+        if(bytes == null)
+        {
+            Debug.Log("what"); 
+        }
+        else
+        {
+            Debug.Log("yep"); 
+    
+        }
+        //Debug.Log(encodedText);
+
+        // tex.LoadRawTextureData(myTexture.EncodeToJPG());
+        // tex.Apply();
+        // pic.texture = tex; 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 3D Attendance System/Assets/RealLifeCamera.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+
+public class RealLifeCamera : MonoBehaviour
+{
+    private bool camAvailable; 
+    private WebCamTexture deviceCam;
+    Texture defaultBackground; 
+    public RawImage background; 
+
+    public AspectRatioFitter fit; 
+    float scaleY; 
+
+    void Start()
+    {
+        defaultBackground = background.texture;
+        WebCamDevice[] devices = WebCamTexture.devices; 
+
+        if(devices.Length == 0 )
+        {
+            //no cam detected. no need for image. 
+            camAvailable = false;
+            return; 
+        }
+        
+        for(int i=0; i < devices.Length ; i++)
+        {
+            if(devices[i].isFrontFacing)
+            {
+                deviceCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height); 
+            }
+        }
+
+        deviceCam.Play();
+        background.texture = deviceCam; 
+
+        camAvailable = true;
+
+    }
+
+    void Update()
+    {
+        if(!camAvailable)
+        {
+            return; 
+        }
+        
+        float ratio = (float)deviceCam.width / (float)deviceCam.height;
+        fit.aspectRatio = ratio; 
+
+        if(deviceCam.videoVerticallyMirrored)
+        {
+            scaleY = -1; 
+        }
+        else
+        {
+            scaleY = 1; 
+        }
+
+        background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+
+        int orient = -deviceCam.videoRotationAngle; 
+        background.rectTransform.localEulerAngles = new Vector3(0,0, orient);
+
+    }
+}
